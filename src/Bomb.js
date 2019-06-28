57f4c02721cd53721b732f7f5fe9f90f75ee5109
Sean Watters <sean@watters.io>
2019-06-28 09:52:08 -0700

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+  state = {
+    secondsLeft: this.props.initialCount
+  }
+
+  render() {
+    const message = this.state.secondsLeft == 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+    return (
+      <div>{message}</div>
+    )
+  }
+
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,17 @@
-// your ImageSlider code here!
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+
+  state = {
+    currentSlideIndex: 0
+  }
+
+  render() {
+    return (
+      <h1>I am on slide {this.state.currentSlideIndex}</h1>
+    )
+  }
+
+}
+
+export default ImageSlider;
